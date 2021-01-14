739c3c65469bc05d198b0b870ee6f8b42fcd51cc
Sarunas Verbus <sarunas@verbus.lt>
2021-01-14 23:08:13 +0200

    Add db data seed file

--- backend/src/main/java/lt/verbus/backend/controller/ProductController.java
+++ backend/src/main/java/lt/verbus/backend/controller/ProductController.java
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
